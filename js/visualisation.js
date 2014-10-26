eb3cb3dc53434201b3f96225526e9bf6e0ad5ff8
Bamdad Dashtban <Bamdad@thoughtworks.com>
2014-10-26 05:03:21 +0000

    Gamify

--- js/visualisation.js
+++ js/visualisation.js
@@ -3,6 +3,29 @@ var notesToVisualise = [];
 var keyboard = Keyboard(document);
 var mono = MonoSynth(keyboard, 1);
 
+function Game(notes, keyboard){
+    var notes_to_play = (_.map(notes, function(i){return i.noteNumber}));
+    var notes_played = [];
+    var note_index = 0;
+    var score = 0;
+
+    var updateScore= function(){
+        $('#score').text(score);
+    }
+    console.log(notes_to_play);
+    keyboard.addEventListener('note_down', function(note){
+        console.log(note);
+        if(notes_to_play[note_index] == note){
+            score  = score+10;
+            updateScore();
+            note_index++;
+        }
+        else{
+//            alert('You suck at this game');
+        }
+    })
+}
+
 var gantt;
 function drawChart() {
     $('.chart').remove();
@@ -46,10 +69,10 @@ function drawChart() {
     // second_half = notesToVisualise.slice(notesToVisualise.length / 2, notesToVisualise.length - 1);
 
     gantt(notesToVisualise);
+    Game(notes, keyboard);
 
     var i = 0;
     var move = function() {
-        console.log("redraw");
         gantt.redraw(notesToVisualise);
         setTimeout(function () {
             if(!gantt.atEnd()) move();
@@ -60,6 +83,4 @@ function drawChart() {
     move();
 
 }
-
-
 
