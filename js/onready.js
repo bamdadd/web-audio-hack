d3325d270b15172674bae95d3afee061fb88a69f
Bamdad Dashtban <Bamdad@thoughtworks.com>
2014-10-26 04:18:10 +0000

    got rid of midi drag and drop

--- js/onready.js
+++ js/onready.js
@@ -8,25 +8,7 @@ $(document).ready(function() {
         document.addEventListener('dragenter', cancelEvent, false);
         document.addEventListener('dragover', cancelEvent, false);
         document.addEventListener('drop', function(e){
-            cancelEvent(e);
-            for(var i=0;i<e.dataTransfer.files.length;++i){
-                var
-                    file = e.dataTransfer.files[i]
-                    ;
-                if(file.type != 'audio/midi'){
-                    continue;
-                }
-                var
-                    reader = new FileReader()
-                    ;
-                reader.onload = function(e){
-                    midiFile = MidiFile(e.target.result);
-                    synth = Synth(44100);
-                    replayer = Replayer(midiFile, synth);
-                    audio = AudioPlayer(replayer);
-                };
-                reader.readAsBinaryString(file);
-            }
+        cancelEvent(e);
         }, false);
     }
 });
